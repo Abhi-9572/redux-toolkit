1454c6e5fa7a76ec1d1246f70216e3c75b027376
Abhishek Raj <abhishekraj19992@gmail.com>
2024-10-06 18:29:28 +0530

    redux toolkit with talwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from './components/Todo'
+import { Provider } from 'react-redux'
+import AddTodo from './components/AddTodo'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
    
-   <h1>REDUX toolkit</h1>
+    <AddTodo/>
+   <Todos/> 
     </>
   )
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../feature/todo/todoSlice';
 
-export const store=configureStore({}); 
+export const store = configureStore({
+    reducer: todoReducer
+})
 
 /**
  * configureStore is use to create store
